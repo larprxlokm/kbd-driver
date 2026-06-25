d26c9a054a2bae7be7336ecc13d2dc232b215dde
rxlok <xyznuts@gmail.com>
2026-06-25 15:36:16 +0200

    Refactor keyboard driver function declarations

--- kbd-driver.c
+++ kbd-driver.c
@@ -1,7 +1,11 @@
-#ifndef KBD_DRIVER_H
-#define KBD_DRIVER_H
-
 #include <stdint.h>
+
+#define KEYBOARD_DATA_PORT 0x60
+#define KEYBOARD_STATUS_PORT 0x64
+
+typedef void (*key_callback_t)(uint8_t scancode);
+
+static key_callback_t key_handler = 0;
 
 static inline uint8_t inb(uint16_t port) {
     uint8_t ret;
@@ -13,14 +17,7 @@ static inline void outb(uint16_t port, uint8_t val) {
     __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
 }
 
-#define KEYBOARD_DATA_PORT 0x60
-#define KEYBOARD_STATUS_PORT 0x64
-
-typedef void (*key_callback_t)(uint8_t scancode);
-
-static key_callback_t key_handler = 0;
-
-static void keyboard_handler_main() {
+void keyboard_handler_main() {
     uint8_t status = inb(KEYBOARD_STATUS_PORT);
     if (status & 0x01) {
         uint8_t scancode = inb(KEYBOARD_DATA_PORT);
@@ -31,8 +28,6 @@ static void keyboard_handler_main() {
     outb(0x20, 0x20);
 }
 
-static void register_keyboard_callback(key_callback_t handler) {
+void register_keyboard_callback(key_callback_t handler) {
     key_handler = handler;
 }
-
-#endif
